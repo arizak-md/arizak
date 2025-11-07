b6653cb85524ca672ba3c9e1ad67623ad8716949
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 22:49:34 +0300

    Update quote.js

--- plugins/quote.js
+++ plugins/quote.js
@@ -12,7 +12,7 @@ cmd({
     const response = await axios.get("https://api.quotable.io/random");
     const { content, author } = response.data;
 
-    const message = `💬 *"${content}"*\n- ${author}\n\n> *QUOTES BY CRISS AI*`;
+    const message = `💬 *"${content}"*\n- ${author}\n\n> *QUOTES BY ARIZAK-MD*`;
     reply(message);
   } catch (error) {
     console.error("Error fetching quote:", error);
