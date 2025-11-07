70f9a5efc57ed186c1759ce245ec1b49637be1d0
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 23:06:01 +0300

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By ʜᴜɴᴛᴇʀ xᴍᴅ
+> *© Powdered By arizak
 `;
         return reply(weather);
     } catch (e) {
