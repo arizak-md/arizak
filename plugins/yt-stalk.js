9c1e85f683a1041ddfcb83b6df62a98acceb4d7e
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 23:04:58 +0300

    Update yt-stalk.js

--- plugins/yt-stalk.js
+++ plugins/yt-stalk.js
@@ -32,7 +32,7 @@ cmd({
       + `┃🎥 *Videos:* ${yt.video_count}\n`
       + `┃🔗 *Channel Link:* (${yt.channel})\n`
       + `╰━━━⪼\n\n`
-      + `🔹 *Powered by ᴏʙᴇᴅᴛᴇᴄʜ*`;
+      + `🔹 *Powered by arizak*`;
 
     await conn.sendMessage(from, {
       image: { url: yt.avatar },
