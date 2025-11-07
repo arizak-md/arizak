58cd7e70ec2544e77d99c14757cee513be8b9f74
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 23:21:01 +0300

    Update tool-pdf.js

--- plugins/tool-pdf.js
+++ plugins/tool-pdf.js
@@ -12,7 +12,7 @@ cmd({
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        if (!q) return reply("Please provide the text you want to convert to PDF. *Eg* `.topdf` *Pakistan ZindaBad 🇵🇰*");
+        if (!q) return reply("Please provide the text you want to convert to PDF. *Eg* `.topdf` *kenya*");
 
         // Create a new PDF document
         const doc = new PDFDocument();
@@ -25,11 +25,11 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             await conn.sendMessage(from, {
                 document: pdfData,
                 mimetype: 'application/pdf',
-                fileName: 'CrissAI.pdf',
+                fileName: 'ARIZAK.pdf',
                 caption: `
 *📄 PDF created successully!*
 
-> © Created By CRISS AI 💜`
+> © Created By ARIZAK-MD 💜`
             }, { quoted: mek });
         });
 
