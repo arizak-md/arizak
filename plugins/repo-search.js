092b2462d06a5f551be66d11f6e79c8b049cf136
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 22:55:15 +0300

    Update repo-search.js

--- plugins/repo-search.js
+++ plugins/repo-search.js
@@ -25,7 +25,7 @@ cmd({
     responseMsg += `🍴 *Forks*: ${data.forks_count}\n`;
     responseMsg += `👤 *Owner*: ${data.owner.login}\n`;
     responseMsg += `📅 *Created At*: ${new Date(data.created_at).toLocaleDateString()}\n`;
-    responseMsg += `\n> *© Powered by ʜᴜɴᴛᴇʀ xᴍᴅ*`;
+    responseMsg += `\n> *© Powered by ARIZAK-MD*`;
 
     await conn.sendMessage(from, { text: responseMsg }, { quoted: m });
   } catch (error) {
