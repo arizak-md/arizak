ff6d5597f266efdf3da928a196c72765eaaeb3fb
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 23:16:27 +0300

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by ʜᴜɴᴛᴇʀ xᴍᴅ 💜`
+      `> © Uploaded by ARIZAK-MD 💜`
     );
 
   } catch (error) {
