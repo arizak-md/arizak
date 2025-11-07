e4284d01315016c9a1c8c9ee8513734fa9c93ad2
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 22:40:19 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴜɴᴛᴇʀ xᴍᴅ
+  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ARIZAK-MD
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
