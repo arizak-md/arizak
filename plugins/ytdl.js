ed445b276093f71b34c0b1072dd2c8ba43e54059
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 23:01:43 +0300

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -44,7 +44,7 @@ cmd({
             `🔽 *Reply with your choice:*\n` +
             `1.1 *Audio Type* 🎵\n` +
             `1.2 *Document Type* 📁\n\n` +
-            `${config.FOOTER || "𓆩CRISS AI𓆪"}`;
+            `${config.FOOTER || "𓆩ARIZAK-MD𓆪"}`;
 
         const sentMsg = await conn.sendMessage(from, { image: { url: image }, caption: info }, { quoted: mek });
         const messageID = sentMsg.key.id;
