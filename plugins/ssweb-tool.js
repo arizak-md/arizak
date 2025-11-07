aec4e050dcb202e1d20c2c10b6206d2f63c66d61
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 22:58:22 +0300

    Update ssweb-tool.js

--- plugins/ssweb-tool.js
+++ plugins/ssweb-tool.js
@@ -1,4 +1,4 @@
-// code by ⿻ ⌜ 𝐂𝐑𝐈𝐒𝐒 ⌟⿻⃮͛🇵🇰𖤐
+// code by ⿻ ⌜ arizak ⌟⿻⃮͛🇵🇰𖤐
 
 const axios = require("axios");
 const config = require('../config');
@@ -24,21 +24,21 @@ async (conn, mek, m, {
   }
 
   try {
-    // created by jawad tech 
+    // created by glen tech 
     const response = await axios.get(`https://api.davidcyriltech.my.id/ssweb?url=${q}`);
     const screenshotUrl = response.data.screenshotUrl;
 
     // give credit and use
     const imageMessage = {
       image: { url: screenshotUrl },
-      caption: "*WEB SS DOWNLOADER*\n\n> *© Powered By CRISS VEVO*",
+      caption: "*WEB SS DOWNLOADER*\n\n> *© Powered By ARIZAK-MD*",
       contextInfo: {
         mentionedJid: [m.sender],
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363378608564635@newsletter',
-          newsletterName: "CRISS AI SUPPORT",
+          newsletterName: "arizak",
           serverMessageId: 143,
         },
       },
@@ -51,4 +51,4 @@ async (conn, mek, m, {
   }
 });
 
-// ⿻ ⌜ 𝐂𝐑𝐈𝐒𝐒 ⌟⿻⃮͛🇵🇰𖤐
+// ⿻ ⌜ ARIZAK-MD𖤐
