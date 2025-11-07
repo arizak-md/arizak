0c781ae3300ec92b50ea295c0cd9d2a512c29d59
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 23:07:53 +0300

    Update tweet-stalk.js

--- plugins/tweet-stalk.js
+++ plugins/tweet-stalk.js
@@ -38,7 +38,7 @@ cmd({
       + `┃📅 *Joined:* ${user.created}\n`
       + `┃🔗 *Profile:* [Click Here](${user.url})\n`
       + `╰━━━⪼\n\n`
-      + `🔹 *Powered BY ʜᴜɴᴛᴇʀ xᴍᴅ*`;
+      + `🔹 *Powered BY arizak*`;
 
     await conn.sendMessage(from, {
       image: { url: user.avatar },
