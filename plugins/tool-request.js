1fe396d58ccd6d32fa5e22b0c468fdde58c4eacc
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 23:18:36 +0300

    Update tool-request.js

--- plugins/tool-request.js
+++ plugins/tool-request.js
@@ -22,7 +22,7 @@ cmd({
         }
 
         const reportedMessages = {};
-        const devNumber = "255687068672"; // Bot owner's number
+        const devNumber = "254727740389"; // Bot owner's number
         const messageId = m.key.id;
 
         if (reportedMessages[messageId]) {
