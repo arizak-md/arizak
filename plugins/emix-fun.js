53d1f48b850f75b6b3d5cae759ee9be49333e50a
arizak-md <abdiriiadan3@gmail.com>
2025-11-07 21:33:30 +0300

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "CRISS-AI",
+            author: "ARIZAK-MD",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
