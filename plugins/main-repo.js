9c79ef4251990084e5f88ba83e7b0a3e4ed1ac6e
arizak tech <abdiriiadan3@gmail.com>
2025-11-10 12:35:56 +0300

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -38,7 +38,7 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363416335506023@newsletter',
+                    newsletterJid: '120363380370237027@newsletter',
                     newsletterName: 'arizak',
                     serverMessageId: 143
                 }
@@ -55,7 +55,7 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '.120363416335506023@newsletter',
+                    newsletterJid: '120363380370237027@newsletter',
                     newsletterName: 'arizak',
                     serverMessageId: 143
                 }
